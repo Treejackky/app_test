652c7e9db86c07d64383d9830004a9bcd0feec7f
Maoswagger <70340189+Treejackky@users.noreply.github.com>
2025-06-04 14:16:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.get('/', async (req, res) => {
+app.get('/80', async (req, res) => {
   try {
     const startTime = Date.now();
     let secondsElapsed = 0;
@@ -17,7 +17,67 @@ app.get('/', async (req, res) => {
     }, 1000);
 
     // รอ 90 วินาที
-    await new Promise(resolve => setTimeout(resolve, 90000));
+    await new Promise(resolve => setTimeout(resolve, 80000));
+
+    // หยุด interval เมื่อเสร็จแล้ว
+    clearInterval(logInterval);
+
+    const totalTime = Math.floor((Date.now() - startTime) / 1000);
+    console.log(`Completed! Total time: ${totalTime} seconds`);
+
+    res.send({
+      message: 'All Emails Have Been Sent',
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'Something went wrong' });
+  }
+});
+
+
+app.get('/100', async (req, res) => {
+  try {
+    const startTime = Date.now();
+    let secondsElapsed = 0;
+
+    // สร้าง interval สำหรับ log ทุกวินาที
+    const logInterval = setInterval(() => {
+      secondsElapsed = Math.floor((Date.now() - startTime) / 1000);
+      console.log(`Waiting... ${secondsElapsed} seconds elapsed`);
+    }, 1000);
+
+    // รอ 90 วินาที
+    await new Promise(resolve => setTimeout(resolve, 100000));
+
+    // หยุด interval เมื่อเสร็จแล้ว
+    clearInterval(logInterval);
+
+    const totalTime = Math.floor((Date.now() - startTime) / 1000);
+    console.log(`Completed! Total time: ${totalTime} seconds`);
+
+    res.send({
+      message: 'All Emails Have Been Sent',
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'Something went wrong' });
+  }
+});
+
+
+app.get('/120', async (req, res) => {
+  try {
+    const startTime = Date.now();
+    let secondsElapsed = 0;
+
+    // สร้าง interval สำหรับ log ทุกวินาที
+    const logInterval = setInterval(() => {
+      secondsElapsed = Math.floor((Date.now() - startTime) / 1000);
+      console.log(`Waiting... ${secondsElapsed} seconds elapsed`);
+    }, 1000);
+
+    // รอ 90 วินาที
+    await new Promise(resolve => setTimeout(resolve, 120000));
 
     // หยุด interval เมื่อเสร็จแล้ว
     clearInterval(logInterval);
